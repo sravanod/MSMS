ffb3776d4b32c802ae4309bb84e915f12b980064
sravanod <sravan125.vtalent@gmail.com>
2019-09-05 14:28:30 +0530

    MSMS Project Added to GitHub

---
+++ MSMS/MSMS/BusinessAccessLayer/BAL.cs
@@ -0,0 +1,17 @@
+﻿using MSMS.IRepositary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSMS.BusinessAccessLayer
+{
+    public class BAL
+    {
+        IReposit IObj;
+        public BAL(IReposit _IObj)
+        {
+            IObj = _IObj;
+        }
+    }
+}
---
+++ MSMS/MSMS/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using MSMS.BusinessAccessLayer;
+using MSMS.Repositary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MSMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        BAL ObjBal = new BAL(new Reposit());
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
